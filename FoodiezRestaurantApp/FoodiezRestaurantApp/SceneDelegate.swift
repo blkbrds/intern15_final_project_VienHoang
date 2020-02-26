c102b46b4f0359200d78d3848d958a069d548826
Vien Hoang <vien.hoang@asiantech.vn>
2020-02-26 10:36:47 +0700

    Setup project

--- FoodiezRestaurantApp/FoodiezRestaurantApp/SceneDelegate.swift
+++ FoodiezRestaurantApp/FoodiezRestaurantApp/SceneDelegate.swift
@@ -8,46 +8,73 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    enum TypeScreen {
+        case tabbar
+    }
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        window.makeKeyAndVisible()
+
+        changeScreen(type: .tabbar)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
     }
 
+    private func createTabbar() {
 
+        let homeVC = HomeViewController()
+        let homeNavi = UINavigationController(rootViewController: homeVC)
+        homeNavi.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "home"), selectedImage: UIImage(named: "home"))
+
+        let messagesVC = FavoritesViewController()
+        let messagesNavi = UINavigationController(rootViewController: messagesVC)
+        messagesNavi.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "favorite"), selectedImage: UIImage(named: "favorite"))
+
+        let friendsVC = SearchViewController()
+        let friendsNavi = UINavigationController(rootViewController: friendsVC)
+        friendsNavi.tabBarItem = UITabBarItem(title: "Search", image: UIImage(named: "search"), tag: 2)
+
+        let profileVC = TrendingViewController()
+        let profileNavi = UINavigationController(rootViewController: profileVC)
+        profileNavi.tabBarItem = UITabBarItem(title: "Trending", image: UIImage(named: "trending"), tag: 3)
+
+        let tabbarController = UITabBarController()
+        tabbarController.viewControllers = [homeNavi, messagesNavi, friendsNavi, profileNavi]
+        tabbarController.tabBar.tintColor = .red
+
+        window?.rootViewController = tabbarController
+    }
+
+    func changeScreen(type: TypeScreen) {
+        switch type {
+        case .tabbar:
+            createTabbar()
+        }
+    }
 }
 
+extension SceneDelegate: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        print("Selected Tab : \(tabBarController.selectedIndex)")
+    }
+}
