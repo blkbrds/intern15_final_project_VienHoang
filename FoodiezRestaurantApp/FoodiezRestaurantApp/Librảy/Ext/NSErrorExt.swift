3fa283fddbdc360eb3980b361e9a416f774a5726
Vien Hoang <vien.hoang@asiantech.vn>
2020-03-02 11:35:19 +0700

    add file + add library

---
+++ FoodiezRestaurantApp/FoodiezRestaurantApp/Librảy/Ext/NSErrorExt.swift
@@ -0,0 +1,27 @@
+//
+//  NSErrorExt.swift
+//  FoodiezRestaurantApp
+//
+//  Created by user on 3/2/20.
+//  Copyright © 2020 VienH. All rights reserved.
+//
+
+import Foundation
+
+private struct ErrorFieldKey {
+    static var errors = "Errors"
+}
+
+extension NSError {
+
+    var errorsString: [String] {
+        set {
+            objc_setAssociatedObject(self, &ErrorFieldKey.errors, newValue, .OBJC_ASSOCIATION_RETAIN)
+        }
+        get {
+            guard let errors = objc_getAssociatedObject(self, &ErrorFieldKey.errors) as? [String] else { return [] }
+            return errors
+        }
+    }
+}
+
---
+++ FoodiezRestaurantApp/FoodiezRestaurantApp/Model/API/Core/Api.swift
@@ -0,0 +1,37 @@
+//
+//  Api.swift
+//  FoodiezRestaurantApp
+//
+//  Created by user on 3/2/20.
+//  Copyright © 2020 VienH. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+final class Api {
+
+    struct Path {
+        static let baseURL = "https://"
+    }
+}
+
+protocol URLStringConvertible {
+    var urlString: String { get }
+}
+
+protocol ApiPath: URLStringConvertible {
+    static var path: String { get }
+}
+
+private func / (lhs: URLStringConvertible, rhs: URLStringConvertible) -> String {
+    return lhs.urlString + "/" + rhs.urlString
+}
+
+extension String: URLStringConvertible {
+    var urlString: String { return self }
+}
+
+private func / (left: String, right: Int) -> String {
+    return left.appending(path: "\(right)")
+}
